116b928d867c41b0a2b950f481a47efd82b8e5cf
mujeex <mujahid.bappai@yahoo.com>
2019-08-06 17:33:06 +0800

    spelling correction service section

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -23,13 +23,13 @@ const Services= ()=>(
                 delivering quality products at competitive prices. We take the worry out of the process, so you don't have to, giving
                 you the time to take care of other more important things and all for a minimal percentage fee.
             </p>
-            
         </article>
+
         <article className={classes.services_content__article} >
             <h4>Product Development</h4>
             <p>China has the world's most sophisticated production industries, making it the best
                 place to design and develop good quality products at competitive prices. Our team
-                helps source qualified factories, guarantees that your design requirements are not
+                helps source qualified factories, guarantees that your design requirements are met
                 , and manage the entire process including product sample development, OEM branding and 
                 packaging, creating product instruction manuals and delivery to your door-step.
             </p>
@@ -37,21 +37,20 @@ const Services= ()=>(
         </article>
         <article className={classes.services_content__article} >
             <h4>Factory Inspection</h4>
-            <p>At Secure Imex. ensuring product quality is one of our top priorities. We offer pre-shippment inspections to ensure 
-                that the product specifications have been met, the product labelling is done to your requiremnent and the packaging
+            <p>At Secure Imex. ensuring product quality is one of our top priorities. We offer pre-shipment inspections to ensure 
+                that the product specifications have been met, the product labeling is done to your requiremnent and the packaging
                 is up to standard.
-            </p>
-            
+            </p> 
         </article>
+
         <article className={classes.services_content__article} >
             <h4>Shipping</h4>
             <p>Secure Imex manages the entire delivery process from the
             manufacturer in China to your door-step. We take care
             of all the paperwork as well, so you don’t have to go 
             through that stress. We do full container load (FCL)
-            shipment and groupie using reliable China shipping
+            shipment and groupage using reliable shipping
             lines.</p>
-            
         </article>
     </>
     }
