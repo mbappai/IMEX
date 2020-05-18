82fc73f173373577eda8334dd3c80570f2aba313
mujeex <mujahid.bappai@yahoo.com>
2020-05-18 07:51:22 +0100

    style: Fix header nav styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,11 +9,11 @@ const Header=(props)=>(
           <div className={classes.header__logo}><LogoSvg /></div>
           <div className={classes.header__hamburger}><Hamburger onclick={props.showSideBar}/></div>
           <ul className={classes.header__nav}>
-          <li><Link className={classes.linkStyle} to="/#expertise">Expertise</Link></li>
           <li><Link className={classes.linkStyle} to="/#services">Services</Link></li>
+          <li><Link className={classes.linkStyle} to="/#expertise">Expertise</Link></li>
           <li><Link className={classes.linkStyle} to="/#pricing">Pricing</Link></li>
-          <li><Link activeClassName={classes.activeLink}   to="/about">About</Link></li>
-          <li><Link activeClassName={classes.activeLink} to="/contact-us">Contact</Link></li>
+          <li><Link activeClassName={classes.activeLink} className={classes.linkStyle}  to="/about">About</Link></li>
+          <li><Link activeClassName={classes.activeLink} className={classes.linkStyle} to="/contact-us">Contact</Link></li>
           </ul>
         </header>
 )
